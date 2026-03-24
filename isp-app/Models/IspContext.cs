9b06bdd52f5759840b41a27855db142b5f7b50a5
NkvXness <leonakavexmusic@gmail.com>
2026-03-24 20:24:52 +0300

    feat: настроен DbContext и строка подключения

---
+++ isp-app/Models/IspContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace IspApp.Models
+{
+    public class IspContext : DbContext
+    {
+        public IspContext(DbContextOptions<IspContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Subscriber> Subscribers { get; set; }
+        public DbSet<Tariff> Tariffs { get; set; }
+        public DbSet<Equipment> Equipment { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+        public DbSet<SupportRequest> SupportRequests { get; set; }
+    }
+}
