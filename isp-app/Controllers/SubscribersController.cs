ad04adc0d11ea62923220b9dafbdac457dc3698e
NkvXness <leonakavexmusic@gmail.com>
2026-03-26 18:02:44 +0300

    feat: создан SubscribersController

---
+++ isp-app/Controllers/SubscribersController.cs
@@ -0,0 +1,28 @@
+﻿using IspApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace IspApp.Controllers
+{
+    public class SubscribersController : Controller
+    {
+        private readonly IspContext _db;
+
+        public SubscribersController(IspContext db)
+        {
+            _db = db;
+        }
+
+        [ResponseCache(CacheProfileName = "Default")]
+        public IActionResult Index()
+        {
+            // подгрузка тарифа сразу, чтобы не делать отдельные запросы для каждой строки
+            var subscribers = _db.Subscribers
+                .Include(s => s.Tariff)
+                .OrderBy(s => s.FullName)
+                .ToList();
+
+            return View(subscribers);
+        }
+    }
+}
