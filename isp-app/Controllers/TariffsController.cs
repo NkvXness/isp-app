35579c215ece02cf988bc30e5061d6bd535c7bbd
NkvXness <leonakavexmusic@gmail.com>
2026-03-26 18:19:08 +0300

    feat: создан TariffsController

---
+++ isp-app/Controllers/TariffsController.cs
@@ -0,0 +1,26 @@
+﻿using IspApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IspApp.Controllers
+{
+    public class TariffsController : Controller
+    {
+        private readonly IspContext _db;
+
+        public TariffsController(IspContext db)
+        {
+            _db = db;
+        }
+
+        [ResponseCache(CacheProfileName = "Default")]
+        public IActionResult Index()
+        {
+            var tariffs = _db.Tariffs
+                .OrderBy(t => t.TariffType)
+                .ThenBy(t => t.MonthlyPrice)
+                .ToList();
+
+            return View(tariffs);
+        }
+    }
+}
