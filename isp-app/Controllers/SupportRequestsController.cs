7acde4760dfffec78e45b9ecb8069c1f9da859e4
NkvXness <leonakavexmusic@gmail.com>
2026-03-27 16:54:09 +0300

    feat: создан SupportRequestsController

---
+++ isp-app/Controllers/SupportRequestsController.cs
@@ -0,0 +1,27 @@
+﻿using IspApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace IspApp.Controllers
+{
+    public class SupportRequestsController : Controller
+    {
+        private readonly IspContext _db;
+
+        public SupportRequestsController(IspContext db)
+        {
+            _db = db;
+        }
+
+        [ResponseCache(CacheProfileName = "Default")]
+        public IActionResult Index()
+        {
+            var requests = _db.SupportRequests
+                .Include(r => r.Subscriber)
+                .OrderByDescending(r => r.RequestDate)
+                .ToList();
+
+            return View(requests);
+        }
+    }
+}
