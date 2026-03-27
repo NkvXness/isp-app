2343019fbea2c961aa6bb83f67b9ce454536c53e
NkvXness <leonakavexmusic@gmail.com>
2026-03-27 16:08:23 +0300

    feat: создан EquipmentController

---
+++ isp-app/Controllers/EquipmentController.cs
@@ -0,0 +1,28 @@
+﻿using IspApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace IspApp.Controllers
+{
+    public class EquipmentController : Controller
+    {
+        private readonly IspContext _db;
+
+        public EquipmentController(IspContext db)
+        {
+            _db = db;
+        }
+
+        [ResponseCache(CacheProfileName = "Default")]
+        public IActionResult Index()
+        {
+            var equipment = _db.Equipment
+                .Include(e => e.Subscriber)
+                .OrderBy(e => e.EquipmentType)
+                .ThenBy(e => e.Model)
+                .ToList();
+
+            return View(equipment);
+        }
+    }
+}
