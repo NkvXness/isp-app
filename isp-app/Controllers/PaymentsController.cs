5ecd8902f5460fd6b75496f3749fd47d47b00d98
NkvXness <leonakavexmusic@gmail.com>
2026-03-27 16:31:47 +0300

    feat: создан PaymentsController

---
+++ isp-app/Controllers/PaymentsController.cs
@@ -0,0 +1,27 @@
+﻿using IspApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace IspApp.Controllers
+{
+    public class PaymentsController : Controller
+    {
+        private readonly IspContext _db;
+
+        public PaymentsController(IspContext db)
+        {
+            _db = db;
+        }
+
+        [ResponseCache(CacheProfileName = "Default")]
+        public IActionResult Index()
+        {
+            var payments = _db.Payments
+                .Include(p => p.Subscriber)
+                .OrderByDescending(p => p.PaymentDate)
+                .ToList();
+
+            return View(payments);
+        }
+    }
+}
