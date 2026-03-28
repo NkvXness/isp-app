275ca233191b38c11387563d456f84350ce80fa2
NkvXness <leonakavexmusic@gmail.com>
2026-03-28 15:44:07 +0300

    feat: добавлен ViewModel для статистики тарифов

---
+++ isp-app/Models/ViewModels/TariffStatsViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace IspApp.Models.ViewModels
+{
+    public class TariffStatsViewModel
+    {
+        public string TariffName { get; set; } = "";
+        public string TariffType { get; set; } = "";
+        public int Speed { get; set; }
+        public decimal MonthlyPrice { get; set; }
+        public int SubscriberCount { get; set; }
+    }
+}
